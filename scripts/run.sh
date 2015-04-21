98739fcdf7997ace2ad667b7a4dfc4c804f10ff0
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-04-21 13:45:12 +0200

    Resarts and removes old container

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,6 +22,13 @@ else
     fi
 fi
 
+if ${sudo} docker ps | awk '{print $NF}' | grep -qx ${name}; then
+    echo "$0: Docker container with name $name already running. Press enter to restart it, or ctrl+c to abort."
+    read foo
+    ${sudo} docker kill ${name}
+fi
+$sudo docker rm ${name}
+
 ${sudo} docker run --rm=true \
     --name ${name} \
     -v $PWD/etc:/opt/pefim/etc \
